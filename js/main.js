f6467c7f581a97c922cbb271dc51fa3f0284168b
Kyle <kkingsbe@gmail.com>
2019-12-20 16:18:47 -0500

    changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let joints = []
 let members = []
 let newJoint = drawNewJoint()
+let newMember;
 let canvas = document.getElementById("canvas")
 
 init()
@@ -16,12 +17,22 @@ $(".viewport").mousemove((e) => {
 
   newJoint.setAttribute("cx", e.pageX)
   newJoint.setAttribute("cy", y)
+
+  if(typeof(newMember) !== "undefined")
+  {
+    newMember.setAttribute("x2", e.pageX)
+    newMember.setAttribute("y2", y)
+  }
 })
 
 $(".viewport").click(() => {
   document.getElementById("newJointCircle").setAttribute("id", `joint-${joints.length}`)
+  joints.push(newJoint)
   newJoint = drawNewJoint()
+  newMember = drawNewMember()
+  console.log(newMember)
   canvas.appendChild(newJoint)
+  canvas.appendChild(newMember)
 })
 
 function makeSVG(tag, attrs) {
@@ -34,4 +45,9 @@ function makeSVG(tag, attrs) {
 function drawNewJoint()
 {
   return makeSVG("circle", {id: "newJointCircle", cx: -100, cy: -100, r:7, stroke: 'black', 'stroke-width': 2, fill: 'red'})
+}
+
+function drawNewMember()
+{
+  return makeSVG("line", {id: "newMember", x1: joints[joints.length-1].getAttribute("cx"), y1: joints[joints.length-1].getAttribute("cy"), x2: "-100", y2: "-100", style: "stroke:rgb(255,0,0);stroke-width:2"})
 }
